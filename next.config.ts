9d7f4886ae1b90263fb5cdba72db3fc0d60cc936
xjathie87 <jany.doe@tutanota.com>
2025-01-15 01:45:03 +0300

    change base path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/im-getting-fired',
   /* config options here */
   sassOptions: {
     silenceDeprecations: ["legacy-js-api"],
