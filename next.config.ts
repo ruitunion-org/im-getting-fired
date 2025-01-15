769d3f042f2dd9510124cddd314ed93a82998f6d
xjathie87 <jany.doe@tutanota.com>
2025-01-15 09:27:32 +0300

    Revert "change base path"
    This reverts commit 9d7f4886ae1b90263fb5cdba72db3fc0d60cc936.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/im-getting-fired',
   /* config options here */
   sassOptions: {
     silenceDeprecations: ["legacy-js-api"],
